007e9cb194603a3c70d5117e2e45d3b341241329
Brok Bucholtz <brok.bucholtz@gmail.com>
2015-08-11 15:23:55 -0700

    Issue #9: Fix bug with multilevel directories

--- operations/setup_server.js
+++ operations/setup_server.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var mkdirp = require('mkdirp');
 var ncp = require('ncp').ncp;
 var submoduleDirectory = './submodules/';
 var serverLibDirectory = './app/server/lib/';
+
+mkdirp(serverLibDirectory);
 
 console.log('Copying submodules to app..');
 ncp(submoduleDirectory + 'endpoints-proto-datastore/endpoints_proto_datastore',
